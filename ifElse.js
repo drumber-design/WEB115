569225813c2d72a88272a6899a271cdc6f65e0c7
drumber-design <60196036+drumber-design@users.noreply.github.com>
2020-01-29 12:36:09 -0500

    Add files via upload

---
+++ ifElse.js
@@ -0,0 +1,10 @@
+var age = parseInt(prompt("Enter your age", 18));
+if(age < 21){
+window.location.replace("http//www.disney.com/");
+}
+else{
+window.location.replace("http//www.espn.com/");
+}
+
+
+    
